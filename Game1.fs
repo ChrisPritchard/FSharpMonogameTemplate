8a21d8c8013601ccaf688211b62650d6e0467282
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-04-30 10:27:44 +1200

    Initial checkin

---
+++ Game1.fs
@@ -0,0 +1,20 @@
+﻿namespace MyGame
+
+    open Microsoft.Xna.Framework
+    open Microsoft.Xna.Framework.Graphics;
+    open Microsoft.Xna.Framework.Input;
+
+    type Game1() as this = 
+        inherit Game()
+
+        let graphics = new GraphicsDeviceManager(this)
+        let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+        override __.LoadContent() = 
+            spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+
+        override __.Update(gameTime) =
+            ()
+
+        override __.Draw(gameTime) =
+            this.GraphicsDevice.Clear Color.CornflowerBlue
---
+++ Program.fs
@@ -0,0 +1,9 @@
+﻿module MyGameEntry
+
+open MyGame
+
+[<EntryPoint>]
+let main _ =
+    use game = new Game1()
+    game.Run()
+    0
